069248b144ac6beac0c538dd936a2471be5b5aa9
Alexander Kleymenov <alexander.kleymenov@gmail.com>
2011-10-04 15:30:59 +0700

    initial import of groovy launcher

---
+++ gr.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set GROOVY_VER=1.8.2
+::set JAVA_DEBUG=-verbose:class -Dgroovy.grape.report.downloads=true 
+set JAVA_OPTS=-Dfile.encoding=utf8 %JAVA_OPTS%
+
+set BASE=%~dp0
+%JAVA_HOME%\bin\java %JAVA_OPTS% %JAVA_DEBUG% -cp "%BASE%lib\*" ^
+    -Dgrape.root="%BASE% " ^
+    -Dgroovy.version=%GROOVY_VER% ^
+    groovy.ui.GroovyMain "%BASE%groovy.gr" %*
