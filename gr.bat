b6ba7c2f4e7e0f7683b1e1edd495b43b528a866f
Alexander Kleymenov <alexander.kleymenov@gmail.com>
2012-01-07 05:53:48 +0600

    automatic sl4j log injection has been implemented

--- gr.bat
+++ gr.bat
@@ -2,10 +2,12 @@
 
 set GROOVY_VER=1.8.2
 ::set JAVA_DEBUG=-verbose:class -Dgroovy.grape.report.downloads=true 
+::set JAVA_DEBUG=-Dgroovy.grape.report.downloads=true 
 set JAVA_OPTS=-Dfile.encoding=utf8 %JAVA_OPTS%
 
 set BASE=%~dp0
 %JAVA_HOME%\bin\java %JAVA_OPTS% %JAVA_DEBUG% -cp "%BASE%lib\*" ^
-    -Dgrape.root="%BASE% " ^
+    -Dgroovy.base="%BASE%." ^
+    -Dgrape.root="%BASE%." ^
     -Dgroovy.version=%GROOVY_VER% ^
     groovy.ui.GroovyMain "%BASE%groovy.gr" %*
